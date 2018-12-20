9be91531d7d4db64e05bf2a683bacfd859178747
Luke Skywalker <joshua.mobley.jm@gmail.com>
2018-12-19 21:05:22 -0700

    map now showing on page in designated div

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,22 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBU1EMgzBxJD5sxWe0T5iIaBQVh3TysXEg",
-    authDomain: "projectone-6b0f1.firebaseapp.com",
-    databaseURL: "https://projectone-6b0f1.firebaseio.com",
-    projectId: "projectone-6b0f1",
-    storageBucket: "",
-    messagingSenderId: "872777787634"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBU1EMgzBxJD5sxWe0T5iIaBQVh3TysXEg",
+  authDomain: "projectone-6b0f1.firebaseapp.com",
+  databaseURL: "https://projectone-6b0f1.firebaseio.com",
+  projectId: "projectone-6b0f1",
+  storageBucket: "",
+  messagingSenderId: "872777787634"
+};
+firebase.initializeApp(config);
 
-  
+var apiKey = 'AIzaSyBRjrg9AAPsYYExgfenyhrdUp_szEucXZ4';
+
+function initMap() {
+  // The location of Phoenix
+  var Phoenix = {lat: 33.4484, lng: -112.0740};
+  // The map, centered at Phoenix
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 10, center: Phoenix});
+  // The marker, positioned at Phoenix
+  var marker = new google.maps.Marker({position: Phoenix, map: map});
+}
