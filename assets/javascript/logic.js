efb68a5b803364e6598ef397d5d9d757e183d1b8
Luke Skywalker <joshua.mobley.jm@gmail.com>
2019-01-02 19:25:49 -0700

    added functionality for storing zip code that is entered

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,18 +9,16 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var zip;
+
+$('#zipsubmit').click(function() {
+  event.preventDefault();
+
+  zip = $('#zipinput').val();
+  console.log(zip);
+});
+
 var apiKey = 'AIzaSyBRjrg9AAPsYYExgfenyhrdUp_szEucXZ4';
-
-function initMap() {
-  // The location of Phoenix
-  var Phoenix = {lat: 33.4484, lng: -112.0740};
-  // The map, centered at Phoenix
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 10, center: Phoenix});
-  // The marker, positioned at Phoenix
-  var marker = new google.maps.Marker({position: Phoenix, map: map});
-};
-
 
 queryURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.4484,-112.0740&radius=1500&keyword=dog+park&key=' + apiKey;
 
@@ -31,4 +29,5 @@ $.ajax({
   console.log(response);
   $('#map').append(response.results[0].name);
 });
+
 
