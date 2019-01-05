dd7c7866f145a4757db9e78929970aae4b658c04
Luke Skywalker <joshua.mobley.jm@gmail.com>
2019-01-05 12:43:44 -0700

    added further functions to page, and added comments to logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,18 +9,26 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Global vars 
 var zip;
 var apiKey = 'AIzaSyBRjrg9AAPsYYExgfenyhrdUp_szEucXZ4';
 
+// Click function for when user submits zip
 $('#zipsubmit').click(function () {
-  event.preventDefault();
 
+  // Prevents form from submitting and clears previous content
+  event.preventDefault();
+  $('#parks').html("");
+  // Pulls the value of the input
   zip = $('#zipinput').val();
+
+  // Vars used in calculations
   var geoURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zip + '&key=' + apiKey;
   var lat;
   var lng;
   var latlng;
 
+  // API call to the google Geocoding
   $.ajax({
     url: geoURL,
     method: "GET"
@@ -30,15 +38,19 @@ $('#zipsubmit').click(function () {
 
     latlng = lat + "," + lng;
 
+    // Var used for the Google Places API
     var queryURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latlng + '&radius=1500&keyword=dog+park&key=' + apiKey;
 
+    // API call to the Places API to find parks within specified zip code
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
+      // If response comes back with nothing displays to DIV
       if (response.status == "ZERO_RESULTS") {
         $('#parks').html('NO PARKS IN YOUR AREA');
       }
+      // Displays response to correct DIV
       else {
 
         for (var i = 0; i < response.results.length; i++) {
@@ -50,12 +62,6 @@ $('#zipsubmit').click(function () {
           $('#parks').append(parkdiv);
         }
       }
-      console.log(response);
     });
   });
 });
-
-
-
-
-
